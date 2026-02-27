e5d85b641d9d3f4849be616cda1bb08a789aa4c4
touhidcse <touhidcucse38@gmail.com>
2026-02-28 01:13:46 +0600

    products loading on product page

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
 console.log("connected");
 
-const loadAllProducts = async () =>{
-    const URL= "https://fakestoreapi.com/products";
-    const res = await fetch(URL);
-    const data =await res.json();
-    diplayProducts(data);
+
+const loadAllProducts= ()=>{
+    fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        disPlayProducts(data);
+    });
 }
 
-const diplayProducts = (products) =>{
-    const productContainer = document.getElementById("products-container");
-    productContainer.innerHTML="";
-    postMessage.forEach(products =>{
-        const li =document.createElement()
+
+
+
+// category
+// : 
+// "men's clothing"
+// description
+// : 
+// "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
+// id
+// : 
+// 1
+// image
+// : 
+// "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png"
+// price
+// : 
+// 109.95
+// rating
+// : 
+// {rate: 3.9, count: 120}
+// title
+// : 
+// "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
+
+const disPlayProducts = (products) =>{
+     //1. Get the container and empty the container
+     const productContainer = document.getElementById("products-container");
+     productContainer.innerHTML="";
     
-    })
+     products.forEach((product) =>{
+        //2. Create HTML Element
+        const productCard = document.createElement("div");
+        productCard.innerHTML=`<div class="product-card">
+            <h2>${product.title}</h2>
+            <p>${product.price}</p>
+        </div>`;
+
+        //3. add to the container
+        productContainer.appendChild(productCard);
+     })
+
 }
+
+loadAllProducts();
