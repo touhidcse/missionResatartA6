70674bc21e5442fa35de2024a7b92f7f79e44a37
touhidcse <touhidcucse38@gmail.com>
2026-02-28 07:30:19 +0600

    image added, all category added

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-console.log("connected");
-
-
-const loadAllProducts= ()=>{
+const loadAllProducts = () => {
     fetch("https://fakestoreapi.com/products")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        disPlayProducts(data);
-    });
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            disPlayProducts(data);
+            // allCategories(data);
+            disPlayCatagories(data);
+        });
 }
-
-
-
-
 // category
 // : 
 // "men's clothing"
@@ -35,23 +30,43 @@ const loadAllProducts= ()=>{
 // : 
 // "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
 
-const disPlayProducts = (products) =>{
-     //1. Get the container and empty the container
-     const productContainer = document.getElementById("products-container");
-     productContainer.innerHTML="";
-    
-     products.forEach((product) =>{
+
+
+const disPlayProducts = (products) => {
+    //1. Get the container and empty the container
+    const productContainer = document.getElementById("products-container");
+    productContainer.innerHTML = "";
+
+    products.forEach((product) => {
         //2. Create HTML Element
         const productCard = document.createElement("div");
-        productCard.innerHTML=`<div class="product-card">
+        productCard.innerHTML = `<div class="product-card">
+            <img src=${product.image}>
+            <p style="display:flex;justify-content: space-between;flex-grow:1"> <span>${product.category} </span> ${product.rating.rate} (${product.rating.count})<span></span></p>
             <h2>${product.title}</h2>
             <p>${product.price}</p>
+            <button>Detalis</button>&nbsp&nbsp <button>Add to Cart</button>
         </div>`;
 
         //3. add to the container
         productContainer.appendChild(productCard);
-     })
+    })
 
 }
 
-loadAllProducts();
+// display category
+
+const disPlayCatagories = (products) => {
+    console.log("IN Display Button function")
+    const categoriesContainer = document.getElementById("categories-container");
+    categoriesContainer.innerHTML = "";
+    products.forEach((product) => {
+        const categoryButton = document.createElement("button");
+        categoryButton.innerHTML = product.category;
+        categories= product.category;
+        console.log(product.category);
+        categoriesContainer.appendChild(categoryButton);
+    });
+}
+
+
